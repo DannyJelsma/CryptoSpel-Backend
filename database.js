81be34746b871060d3d1cbbcbea6f288021a584d
Danny Jelsma <danny.jelsma@hotmail.com>
2021-06-02 15:26:01 +0200

    MongoDB url

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb+srv://root:SiCGyJeGcW23CcMl@cluster0.9cden.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://root:SiCGyJeGcW23CcMl@cluster0.9cden.mongodb.net/Project?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = client;
