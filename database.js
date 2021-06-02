69afe69f4337cf1cefb1eded8548ad322e40ead5
Martijn <viziousdev@gmail.com>
2021-06-02 16:41:24 +0200

    Added mongoose.

--- database.js
+++ database.js
@@ -1,14 +1,27 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb+srv://root:SiCGyJeGcW23CcMl@cluster0.9cden.mongodb.net/cryptospel?retryWrites=true&w=majority';
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = 'mongodb+srv://root:SiCGyJeGcW23CcMl@cluster0.9cden.mongodb.net/cryptospel?retryWrites=true&w=majority';
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let instance;
-module.exports = {
-  connect: function (callback) {
-    client.connect((err, database) => {
-      instance = database.db('cryptospel');
-      return callback(err);
-    });
-  },
-  getInstance: () => instance,
-};
+// let instance;
+// module.exports = {
+//   connect: function (callback) {
+//     client.connect((err, database) => {
+//       instance = database.db('cryptospel');
+//       return callback(err);
+//     });
+//   },
+//   getInstance: () => instance,
+// };
+
+// // mongoose -> https://www.youtube.com/watch?v=b59Pdt5LJG8
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://root:SiCGyJeGcW23CcMl@cluster0.9cden.mongodb.net/cryptospel', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const database = mongoose.connection;
+database.on('error', console.error.bind(console, 'connection error:'));
+
+module.exports = database;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const binance = require('./modules/binance');
 const database = require('./database');
+const models = require('./models');
 
 dotenv.config();
 app.use(express.json());
@@ -12,16 +13,8 @@ const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
-database.connect((err) => {
-  if (err) {
-    console.log(err);
-    process.exit();
-  }
-
+database.once('open', () => {
   app.listen(3000, () => {
     console.log('Server started on port 3000!');
   });
 });
-
-// root
-// SiCGyJeGcW23CcMl
