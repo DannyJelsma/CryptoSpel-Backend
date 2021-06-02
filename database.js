0ea84e0fcc26b36df52b7ccaa41347484bbc53f1
Martijn <viziousdev@gmail.com>
2021-06-02 15:44:33 +0200

    Added MongoDB instance to be used across different files.

--- database.js
+++ database.js
@@ -1,5 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb+srv://root:SiCGyJeGcW23CcMl@cluster0.9cden.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://root:SiCGyJeGcW23CcMl@cluster0.9cden.mongodb.net/cryptospel?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-module.exports = client;
+let instance;
+module.exports = {
+  connect: function (callback) {
+    client.connect((err, database) => {
+      instance = database.db('cryptospel');
+      return callback(err);
+    });
+  },
+  getInstance: () => instance,
+};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const binance = require('./modules/binance');
+const database = require('./database');
 
 dotenv.config();
 app.use(express.json());
@@ -11,8 +12,15 @@ const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000!');
+database.connect((err) => {
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+
+  app.listen(3000, () => {
+    console.log('Server started on port 3000!');
+  });
 });
 
 // root
