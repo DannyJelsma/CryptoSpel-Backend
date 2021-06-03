6df9f55bef5dad3ba0d369facdddfc292d21e98a
Danny Jelsma <danny.jelsma@hotmail.com>
2021-06-03 15:30:47 +0200

    Save coin history in MongoDB

--- modules/binance.js
+++ modules/binance.js
@@ -1,5 +1,5 @@
 const ws = require('ws');
-const fs = require('fs');
+const models = require('../models.js');
 
 initializeWebsockets();
 
@@ -7,49 +7,42 @@ function initializeWebsockets() {
   let client = new ws('wss://stream.binance.com:9443/ws/test');
 
   client.on('close', initializeWebsockets);
-  client.on('message', (msg) => {
-    let updates = JSON.parse(msg);
-
-    for (let update in updates) {
-      let coin = updates[update];
-
-      // Get rid of some invalid values that Binance sends.
-      if (coin === null || coin === 1) {
-        continue;
-      }
-
-      let coinName = coin.s;
-      let price = coin.o;
-
-      if (!coinName.endsWith('EUR')) continue;
-
-      try {
-        let fileName = './coins/' + coinName + '.json';
-        let historyEntry = { date: Date.now(), price: price };
-
-        if (!fs.existsSync('./coins/')) {
-          fs.mkdirSync('./coins/');
-        }
-
-        if (fs.existsSync(fileName)) {
-          let data = fs.readFileSync(fileName, 'utf-8');
-          let json = JSON.parse(data);
-          let lastHistoryEntry = json.history[json.history.length - 1];
-
-          if (lastHistoryEntry.price !== historyEntry.price && Date.now() > new Date(lastHistoryEntry.date + 60000)) {
-            json.history.push(historyEntry);
-            fs.writeFileSync(fileName, JSON.stringify(json));
-          }
-        } else {
-          fs.writeFileSync(fileName, JSON.stringify({ history: [historyEntry] }));
-        }
-      } catch (err) {
-        throw err;
-      }
-    }
-  });
+  client.on('message', handleMessage);
 
   client.once('open', () => {
-    client.send(JSON.stringify({ method: 'SUBSCRIBE', params: ['!ticker@arr'], id: 1 }));
+    client.send(JSON.stringify({method: 'SUBSCRIBE', params: ['!ticker@arr'], id: 1}));
   });
+}
+
+async function handleMessage(msg) {
+  let updates = JSON.parse(msg);
+
+  for (let update in updates) {
+    let coin = updates[update];
+
+    // Get rid of some invalid values that Binance sends.
+    if (coin === null || coin === 1) {
+      continue;
+    }
+
+    let coinName = coin.s;
+    let price = coin.o;
+
+    if (!coinName.endsWith('EUR')) continue;
+
+    let exists = await models.Coin.exists({ticker: coinName});
+
+    let historyEntry = {date: Date.now(), price: price};
+    if (exists) {
+      let coin = await models.Coin.findOne({ticker: coinName});
+      let lastHistoryEntry = coin.history[coin.history.length - 1];
+
+      if (lastHistoryEntry.price !== historyEntry.price && Date.now() > new Date(lastHistoryEntry.date + 60000)) {
+        coin.history.push(historyEntry);
+        coin.save();
+      }
+    } else {
+      await new models.Coin({ticker: coinName, history: [historyEntry]}).save();
+    }
+  }
 }
